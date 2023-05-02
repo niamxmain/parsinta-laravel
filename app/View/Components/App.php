f5294e4a0060b7de3dedd11517a8c753befb1cff
niamx <muhammadniam1999@gmail.com>
2023-05-02 22:45:27 +0700

    custom style in component

--- app/View/Components/App.php
+++ app/View/Components/App.php
@@ -13,6 +13,7 @@ class App extends Component
      */
     
     public $title;
+    public $styles = null;
     public function __construct($title = null)
     {
         $this->title = $title ?? 'niX';
--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,4 +1,11 @@
 <x-app title="About Page">
+    @slot('styles')
+    <style>
+        body {
+            background-color: indigo;
+        }
+    </style>
+    @endslot
     <h3>Konten About</h3>
 </x-app>
 
