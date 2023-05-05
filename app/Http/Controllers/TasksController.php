75b029b46a38895ecb5792d93289bc0c6be5f5a2
niamx <muhammadniam1999@gmail.com>
2023-05-05 21:56:17 +0700

    create model task

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -10,13 +11,14 @@ class TasksController extends Controller
     public function index() 
     {
         // dd(DB::table('tasks')->get());
-        $tasks = DB::table('tasks')->orderBy('id', 'desc')->get();
+        $tasks = Task::orderBy('id', 'desc')->get();
         return view('tasks.index', compact('tasks'));
     }
 
     public function addTask(Request $request) 
     {
-        DB::table('tasks')->insert([
+        // if use [insert] will return bool, and if [create] will return datas
+        Task::create([
             'tugas' => $request->newTask
         ]);
         return back();
@@ -24,20 +26,21 @@ class TasksController extends Controller
 
     public function editTask($id)
     {
+        // use [where]->first() if condition more than 1, and use [find] if just find id 
         return view('tasks.edit', [
-            'task' => DB::table('tasks')->where('id', $id)->first()
+            'task' => Task::find($id),
         ]);
     }
     public function updateTask(Request $request, $id)
     {
-        DB::table('tasks')->where('id', $id)->update([
+        Task::find($id)->update([
             'tugas' => $request->task
         ]);
         return redirect('/tasks');
     }
     public function deleteTask($id) 
     {
-        DB::table('tasks')->delete($id);
+        Task::find($id)->delete();
         return redirect('/tasks');
     }
 }
