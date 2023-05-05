634271f5128ffb9847a22b5beaaed083b2dd9e86
niamx <muhammadniam1999@gmail.com>
2023-05-05 22:38:16 +0700

    edit column mark

--- database/migrations/2023_05_05_135926_add_mark_column_to_tasks_table.php
+++ database/migrations/2023_05_05_135926_add_mark_column_to_tasks_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('tasks', function (Blueprint $table) {
-            $table->enum('mark', ['belum','selesai'])->default('belum')->after('tugas');
+            $table->boolean('mark')->after('tugas')->default(false);
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('tasks', function (Blueprint $table) {
-            $table->enum('mark', ['belum','selesai'])->default('belum')->after('tugas');
+            $table->dropColumn('mark');
         });
     }
 };
