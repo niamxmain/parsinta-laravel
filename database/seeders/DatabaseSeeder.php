5cad7b7eb360fde2f8835c252a4c7cb11ec75a42
niamx <muhammadniam1999@gmail.com>
2023-05-06 23:28:15 +0700

    use foreach to add more than 1 data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
+use App\Models\User;
+use App\Models\Task;
 use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -15,16 +17,26 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        
 
-        \App\Models\User::factory()->create([
-            'name' => 'Niamxmain',
-            'email' => 'test@example.com',
-            'password' => Hash::make('password'),
-            'email_verified_at' => Carbon::now(),
-        ]);
 
-        \App\Models\Task::factory(10)->create();
-
+        $tasks =
+            [
+                [
+                    'name' => 'Niamxmain',
+                    'email' => 'test@example.com',
+                    'password' => Hash::make('password'),
+                    'email_verified_at' => Carbon::now(),
+                ],
+                [
+                    'name' => 'niam',
+                    'email' => 'nix@example.com',
+                    'password' => Hash::make('pass'),
+                    'email_verified_at' => Carbon::now(),
+                ]
+            ];
+        foreach ($tasks as $task) {
+            User::factory()->create($task);
+        }
+        Task::factory(10)->create();
     }
 }
