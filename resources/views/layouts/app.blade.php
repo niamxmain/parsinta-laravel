99781216641fbfeb5fdb836f611af06d5c146a31
niamx <muhammadniam1999@gmail.com>
2023-05-10 23:53:38 +0700

    add bootstrap to project

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{asset ('css/app.css')}}">
+    <link rel="stylesheet" href="{{asset ('css/bootstrap.css')}}">
     {{$styles}}
     <title>{{$title}} | niX</title>
 </head>
@@ -13,6 +13,8 @@
 <body>
     <x-navbar></x-navbar>
     {{$slot}}
+
+    <script src="{{asset('js/bootstrap.min.js')}}"></script>
 </body>
 
 </html>
